793c44469867dccb928241f89ca230fe89ad36b1
Nishant Handa <nh124001@gmail.com>
2016-07-12 14:04:40 +0530

    added JavaStandaloneProjectExtensionTwo with build logging functionality

---
+++ JavaStandaloneProjectExtensionTwo/src/org/nishant/Math.java
@@ -0,0 +1,53 @@
+package org.nishant;
+
+/**
+ * Sample class with simple math operations
+ * 
+ * @author nishant
+ */
+public class Math {
+
+	/**
+	 * To add two positive integers
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	/**
+	 * To subtract two positive integers
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	/**
+	 * To multiply two positive integers
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	/**
+	 * To divide two positive integers
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
