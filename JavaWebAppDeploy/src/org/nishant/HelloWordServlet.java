c4bb50e3f80c64be415fd50ec418730c02d99896
Nishant Handa <nh124001@gmail.com>
2016-07-19 13:29:15 +0530

    added JavaWebAppDeploy project

---
+++ JavaWebAppDeploy/src/org/nishant/HelloWordServlet.java
@@ -0,0 +1,35 @@
+package org.nishant;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWordServlet
+ */
+public class HelloWordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloWordServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+
+		out.println("Hello World From Java Servlet");
+	}
+}
