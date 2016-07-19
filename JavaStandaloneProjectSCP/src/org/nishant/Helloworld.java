411fd7b17b99d24a78a9f1d0d170eceb19573a14
Nishant Handa <nh124001@gmail.com>
2016-07-19 18:17:20 +0530

    added JavaStandaloneProjectSCP project

---
+++ JavaStandaloneProjectSCP/src/org/nishant/Helloworld.java
@@ -0,0 +1,13 @@
+package org.nishant;
+
+/**
+ * Sample Hello World program, just for demonstration
+ * 
+ * @author nishant
+ */
+public class Helloworld {
+
+	public static void main(String[] args) {
+
+	}
+}
